7b72bd3af521b3d8ae82f675cca38faada0682d6
gondaliya akshay <axgondaliya@gmail.com>
2021-07-06 12:12:39 +0530

    setup done'

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    discription="its a wine q package",
+    author="akshay",
+    packages=find_packages(),
+    licence="MIT",
+
+)
